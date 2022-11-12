fc0ca039985c6bc0a614da8470bceeb840710664
ArktThe <96881737+ArktThe@users.noreply.github.com>
2022-11-12 21:34:26 +0100

    Add files via upload

---
+++ zadaca03/Main.java
@@ -0,0 +1,11 @@
+package zadaca03;
+
+public class Main {
+
+public static void main (String[] args) {
+		
+		PC obj1 = new PC ();
+		
+		obj1.zgolemiM(6, 200);
+	}
+}
---
+++ zadaca04/Brojach.java
@@ -0,0 +1,23 @@
+package zadaca04;
+
+public class Brojach {
+
+	int pismo = 0;
+	int glava = 0;
+	
+	public Brojach () {
+		
+	}
+	
+	public void zBrojach(int p0g1) {
+		
+		if (p0g1 == 0) this.pismo++;
+		if (p0g1 == 1) this.glava++;
+		
+	}
+	
+	public void rBrojach() {
+		this.pismo=0;
+		this.glava=0;
+	}
+}
